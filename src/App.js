80b6c03e8da0317b07ecddadb8d107433a7dec6f
Jon Sanders <jonsanders101@aol.com>
2018-09-26 22:17:33 +0100

    Add means to start the game and play the solution sequence

--- src/App.js
+++ src/App.js
@@ -5,15 +5,48 @@ import './App.css';
 
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      currentColourDisplayed: null,
+      solution: [],
+      startButtonText: 'Begin'
+    }
+    this.beginGame = this.beginGame.bind(this);
+  }
+
   render() {
     return (
       <div className="App">
           <h1 class="title">Heather Says...</h1>
-          <button class="button button--begin">Begin</button>
+          <button class="button button--begin" onClick={this.beginGame}>{this.state.startButtonText}</button>
           {COLOURS.map(colour => <ColourButton colour={colour.toLowerCase()}/>)}
-        <div class="prompt-box" id="prompt-box"></div>
+        <div class="prompt-box">{this.state.currentColourDisplayed}</div>
       </div>
     );
+  }
+  beginGame() {
+    this.setState({...this.state, solution: [this.randomColour()], startButtonText: 'Restart'});
+    this.playSolutionSequence();
+  }
+
+  playSolutionSequence() {
+    let timeout = -1500;
+    const timeoutIncrement = 1500;
+    this.state.solution.forEach((colour, index) => {
+        timeout += timeoutIncrement;
+        setTimeout(() => {
+            this.setState({...this.state, currentColourDisplayed: colour});
+        }, timeout)
+
+        if (index === this.state.solution.length - 1) {
+            setTimeout(() => {this.setState({...this.state, currentColourDisplayed: null})}, timeout + timeoutIncrement);
+        }
+    })
+  }
+
+  randomColour() {
+    return COLOURS[Math.floor(Math.random() * (COLOURS.length))]
   }
 }
 
